339848c1e938f4600b2fee1c23f7f7d190f3e260
vagar123 <vanessa.garnica.14@gmail.com>
2020-10-22 18:47:50 -0500

    Proyecto Green life

---
+++ db.js
@@ -0,0 +1,20 @@
+const mongoose = require ('mongoose')//Paquete que permite la comunicación con nuestra base de datos.
+const conectDB = ()=>{
+    //conect permite conectar a una base de datos tiene opciones:
+    // -useNewUrlParser: analizar la informacion que se le quiere enviar a MongoDB.
+    //-useUnifiedTopology escucha llamados que hacemos a mongo de BD y monitoréa que es lo que pasa
+    mongoose.connect('mongodb+srv://Artemisas:greenlife12345@artemisas.juxqx.mongodb.net/Green-Life?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: 
+        true},(error)=>{
+            if(error){
+                console.log('Error',error)
+            
+            }else{
+                console.log('Nos conectamos a la DB.');
+            }
+        })
+}
+/**
+ * module.exports
+ * Permite exportar una funcion para que pueda ser utilizada por otra parte del proyecto
+ */
+module.exports = { conectDB }
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')//Estamos utilizando express en nuestro proyecto.
+const cors = require('cors')
+const bodyparser = require('body-parser')
+
+const { conectDB} = require('./db')
+const app = express()//Se convierte a la cconstante express en un objeto por el cual se va a trabajar
+
+app.use(cors())
+app.use(bodyparser.json())
+
+conectDB()//Ejecutando la conexion a la base de datos
+
+app.listen(3000,() =>{
+    console.log('Bienvenido a GreenLife')
+})
