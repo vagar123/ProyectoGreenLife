c55c1be7e205345fb4c0e01066cfe480dcbc5299
DanielaPF15 <dpfa1507@gmail.com>
2020-10-23 11:31:51 -0500

    COMENTARIO PRUEBA

--- routes/newsletters.js
+++ routes/newsletters.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
     const newsletters = require('../controllers/newsletters')
     app.post('/newsletters/create', newsletters.create)
-    app.put('/newsletters/update/:id', newsletters.update)
+    app.put('/newsletters/update/:id', newsletters.update)/**Enlace actualizar */
 }
