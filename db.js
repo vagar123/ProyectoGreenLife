a489579bee040d18f3ecb85302e4f2abe910f622
DanielaLopez804 <dl7241140@hotmail.com>
2020-11-22 13:55:05 -0500

    Correo funcionando

--- db.js
+++ db.js
@@ -4,9 +4,7 @@ const conectDB = () => {
         //conect permite conectar a una base de datos tiene opciones:
         // -useNewUrlParser: analizar la informacion que se le quiere enviar a MongoDB.
         //-useUnifiedTopology escucha llamados que hacemos a mongo de BD y monitoréa que es lo que pasa
-        mongoose.connect(config.greenlifeDB, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+        mongoose.connect(config.greenlifeDB, { useNewUrlParser: true, useUnifiedTopology: true
         }, (error) => {
             if (error) {
                 console.log('Error', error)
